7b9a094f90d7d7fa2bc4290f9273d250105654b6
Shane Hyde <shane@noctonyx.com>
2021-04-19 01:01:07 +1000

    Check in

--- Triangle/src/Main.cpp
+++ Triangle/src/Main.cpp
@@ -13,6 +13,31 @@
 #include "nlohmann/json.hpp"
 #include "Vfs.h"
 
+void loadAssetList() {
+    std::ifstream f("config.json");
+    nlohmann::json js;
+    f >> js;
+    f.close();
+
+    std::vector<std::filesystem::path> assetPaths;
+
+    auto v = RxAssets::vfs();
+
+    if (js["mounts"].is_array()) {
+        nlohmann::json j2 = js["mounts"];
+        for (auto& j3 : j2) {
+            if (j3.is_object()) {
+                auto path = j3["path"].get<std::string>();
+                auto mount = j3["mount"].get<std::string>();
+
+                v->addMount(path, mount);
+            }
+        }
+    }
+
+    v->scan();
+}
+
 int main()
 {
     try {
@@ -24,28 +49,7 @@ int main()
         spdlog::info("Starting Triangle WIP");
         spdlog::set_level(spdlog::level::debug);
 
-        std::ifstream f("config.json");
-        nlohmann::json js;
-        f >> js;
-        f.close();
-
-        std::vector<std::filesystem::path> assetPaths;
-
-        auto v = RxAssets::vfs();
-
-        if (js["mounts"].is_array()) {
-            nlohmann::json j2 = js["mounts"];
-            for (auto& j3 : j2) {
-                if (j3.is_object()) {
-                    auto path = j3["path"].get<std::string>();
-                    auto mount = j3["mount"].get<std::string>();
-
-                    v->addMount(path, mount);
-                }
-            }
-        }
-
-        v->scan();
+        loadAssetList();
 
         engine_ = std::make_unique<RxEngine::EngineMain>();
         //world_ = std::make_unique<RxEngine::World>(engine_.get());
